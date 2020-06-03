a12ca04d05c94b993e61f1dbbb409599ac24f5f9
nohheekang <nohheekang@gmail.com>
2020-06-04 00:10:27 +0900

    dd

--- Fishing_Game/Fishing_Game/fishing_game.cpp
+++ Fishing_Game/Fishing_Game/fishing_game.cpp
@@ -14,11 +14,13 @@ void test(int size, int x, int y) {
 
 int main() {
 	int temp;
-	/*scanf_s("%d", &temp);
+	scanf_s("%d", &temp);
 	system("cls");
 	test(temp, 0, 0);
-	*/
 
-	gamestart();
+	for (int i = 0; i <= temp; i++) {
+		test(i, 0, 0);
+	}
+	//gamestart();
 	return 0;
 }
