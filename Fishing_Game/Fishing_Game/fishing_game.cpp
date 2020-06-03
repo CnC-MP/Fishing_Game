8d109b49900cba63fc173710ae26296604fab84d
rabbit379 <rabbit379@naver.com>
2020-06-04 00:03:45 +0900

    005

--- Fishing_Game/Fishing_Game/fishing_game.cpp
+++ Fishing_Game/Fishing_Game/fishing_game.cpp
@@ -5,19 +5,26 @@
 #include "monster_fish_image.h"
 
 void test(int size, int x, int y) {
-	monster_fish_image_type01(size, true, x, y, 11, 13, 12);
+	system("cls");
+	monster_fish_image_type01(size, true, x, y, 11, 13, 12, 4);
+	gotoxy(20, 20);
+	printf("%d", size);
 	int temp = _getch();
 	system("cls");
-	monster_fish_image_type01(size, false, x, y, 11, 13, 12);
+	monster_fish_image_type01(size, false, x, y, 11, 13, 12, 4);
+	gotoxy(20, 20);
+	printf("%d", size);
 	temp = _getch();
 }
 
 int main() {
 	int temp;
-	/*scanf_s("%d", &temp);
-	system("cls");
+	scanf_s("%d", &temp);
 	test(temp, 0, 0);
-	*/
+
+	for (int i = 1; i <= temp; i++) {
+		test(i, 0, 0);
+	}
 
 	gamestart();
 	return 0;
