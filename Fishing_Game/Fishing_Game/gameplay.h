1095cefd063972c2ffc2c0c597a2d3cfc7d34867
nohheekang <nohheekang@gmail.com>
2020-06-20 23:33:52 +0900

    dd

--- Fishing_Game/Fishing_Game/gameplay.h
+++ Fishing_Game/Fishing_Game/gameplay.h
@@ -13,10 +13,12 @@
 void gamestart();
 void main_interface_cursor(cursor_location* loc);
 void main_interface_cursor_write(cursor_location loc, int color);
-void difficulty_interface_cursor(cursor_location* loc);
+void difficulty_interface_cursor(cursor_location* loc, int clear_stage);
 void difficulty_interface_cursor_write(cursor_location loc, int color);
+void difficulty_interface_write(int clear_stage);
 void difficulty_interface();
 void main_interface_write02();
+void stage_lock(int x);
 
 void gamestart() {
 	xlimit = 148;
@@ -33,12 +35,14 @@ void gamestart() {
 		main_interface_cursor_write(mainmenu_cursor, 12);
 		main_interface_cursor(&mainmenu_cursor);
 		if (enter_key == 13 && mainmenu_cursor.yPos == 0) { //새로하기
+			enter_key = 0;
 			difficulty_interface();
 		}
 		else if (enter_key == 13 && mainmenu_cursor.yPos == 1) { //이어하기
-			
+			enter_key = 0;
 		}
 		else if (enter_key == 13 && mainmenu_cursor.yPos == 2) { //종료하기
+			enter_key = 0;
 			system("cls");
 			exit(1);
 		}
@@ -69,8 +73,11 @@ void main_interface_cursor_write(cursor_location loc, int color) {
 	color_change(7);
 }
 
-void difficulty_interface_cursor(cursor_location* loc) {
-	horizontal_cursor(loc, 0, 2, 1, 1, 1);
+void difficulty_interface_cursor(cursor_location* loc, int clear_stage) {
+	if (clear_stage > 2) {
+		clear_stage = 2;
+	}
+	horizontal_cursor(loc, 0, clear_stage, 1, 1, 1);
 }
 
 void difficulty_interface_cursor_write(cursor_location loc, int color) {
@@ -84,42 +91,85 @@ void difficulty_interface_cursor_write(cursor_location loc, int color) {
 	color_change(7);
 }
 
-void difficulty_interface_write() {
+void difficulty_interface_write(int clear_stage) { 
 	gotoxy(38, 20);
 	printf("하");
 	gotoxy(74, 20);
 	printf("중");
 	gotoxy(110, 20);
 	printf("상");
+	if (clear_stage < 1) { // yPos는 +-1, xPos는 +-2
+		stage_lock(74);
+	}
+	if (clear_stage < 2) {
+		stage_lock(110);
+	}
 }
 
 void difficulty_interface() {
 	cursor_location difficulty_cursor;
 	difficulty_cursor.xPos = 0;
 	difficulty_cursor.yPos = 0;
+	int clear_stage = 0;
 	while (true) {
 		system("cls");
-		difficulty_interface_write();
+		difficulty_interface_write(clear_stage);
 		difficulty_interface_cursor_write(difficulty_cursor, 12);
-		difficulty_interface_cursor(&difficulty_cursor);
+		difficulty_interface_cursor(&difficulty_cursor, clear_stage);
 		if (enter_key == 13 && difficulty_cursor.xPos == 0) { // 하
+			enter_key = 0;
 			while (true) {
 				system("cls");
-				printf("ㅇ");
 				// 난이도 하 게임 플레이
 			}
 		}
 		else if (enter_key == 13 && difficulty_cursor.xPos == 1) { // 중
+			enter_key = 0;
 			while (true) {
 				system("cls");
 				// 난이도 중 게임 플레이
 			}
 		}
 		else if (enter_key == 13 && difficulty_cursor.xPos == 2) { // 상
+			enter_key = 0;
 			while (true) {
 				system("cls");
 				// 난이도 상 게임 플레이
 			}
 		}
+		else if (close_key == 8) {
+			close_key = 0;
+			break;
+		}
 	}
 }
+
+void stage_lock(int x) {
+	int y = 20;
+	color_change(12);
+	gotoxy(x - 4, y - 2);
+	printf("■");
+	gotoxy(x, y - 2);
+	printf("■");
+	gotoxy(x + 4, y - 2);
+	printf("■");
+	gotoxy(x - 4, y);
+	printf("■");
+	gotoxy(x + 4, y);
+	printf("■");
+	gotoxy(x - 4, y + 2);
+	printf("■");
+	gotoxy(x, y + 2);
+	printf("■");
+	gotoxy(x + 4, y + 2);
+	printf("■");
+	color_change(7);
+}
+
+/*void gamestart() {
+	// 플레이어 목숨
+	// 몬스터 물고기 리젠 타임
+	// 몬스터 물고기 움직이는 속도
+	// 물고기 피격 판정
+	// 
+}*/
